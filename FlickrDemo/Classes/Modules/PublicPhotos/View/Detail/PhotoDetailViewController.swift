705afea8f08b709e04944c0e6e52ecf9857196cf
John McIntosh <johntmcintosh@gmail.com>
2017-10-03 22:23:47 -0500

    Initial stub creation of view controller for displaying a photo's detail view

---
+++ FlickrDemo/Classes/Modules/PublicPhotos/View/Detail/PhotoDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  PhotoDetailViewController.swift
+//  FlickrDemo
+//
+//  Created by John McIntosh on 10/3/17.
+//  Copyright © 2017 John T McIntosh. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotoDetailViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var imageView: UIImageView! {
+        didSet {
+            guard animated else {
+                guard let data = try? Data(contentsOf: photo.large) else { return }
+                imageView.image = UIImage(data: data)
+                return
+            }
+        }
+    }
+    @IBOutlet weak var titleContainer: UIView!
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.text = photo.title
+        }
+    }
+    
+    
+    // MARK: Properties
+    
+    let photo: PhotoDisplayable
+    let animated: Bool
+    
+    
+    // MARK: Initializers
+    
+    init(photo: PhotoDisplayable, animated: Bool = true) {
+        self.photo = photo
+        self.animated = animated
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
